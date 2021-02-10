8a507fb504de54a7c173b81b528f1ec5655980e6
Dmitrii Poliakov <Dmitrii.Poliakov@tdameritrade.com>
2021-02-10 13:42:35 +0100

    Introduce ContinuousQueryWithTransformer instead of basic ContinuousQuery to reduce amount of data sent over network

--- src/main/java/com/devexperts/server/CacheLocalKeysTracker.java
+++ src/main/java/com/devexperts/server/CacheLocalKeysTracker.java
@@ -4,14 +4,19 @@ import com.devexperts.common.CacheInfo;
 import com.devexperts.common.Heartbeat;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.cache.affinity.Affinity;
-import org.apache.ignite.cache.query.ContinuousQuery;
+import org.apache.ignite.cache.query.ContinuousQueryWithTransformer;
 import org.apache.ignite.cache.query.QueryCursor;
 import org.apache.ignite.cache.query.ScanQuery;
 import org.apache.ignite.cluster.ClusterNode;
+import org.apache.ignite.lang.IgniteClosure;
+import org.jetbrains.annotations.NotNull;
 
 import javax.cache.Cache;
+import javax.cache.configuration.Factory;
+import javax.cache.configuration.FactoryBuilder;
 import javax.cache.event.CacheEntryEvent;
 import javax.cache.event.EventType;
+import java.io.Serializable;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -45,12 +50,17 @@ public class CacheLocalKeysTracker {
     }
 
     private synchronized void startCacheListener() {
-        //dpo todo 09.02.21 Remote Transformer
-        ContinuousQuery<String, Heartbeat> query = new ContinuousQuery<>();
+        ContinuousQueryWithTransformer<String, Heartbeat, CacheTrackerEvent> query =
+                new ContinuousQueryWithTransformer<>();
+        Factory<IgniteClosure<CacheEntryEvent<? extends String, ? extends Heartbeat>, CacheTrackerEvent>> factory =
+                FactoryBuilder.factoryOf(
+                        (IgniteClosure<CacheEntryEvent<? extends String, ? extends Heartbeat>, CacheTrackerEvent>)
+                        CacheTrackerEvent::new);
+        query.setRemoteTransformerFactory(factory);
         query.setInitialQuery(new ScanQuery<>());
         query.setLocalListener(events -> {
-            for (CacheEntryEvent<? extends String, ? extends Heartbeat> event : events) {
-                processKey(event.getKey(), event.getEventType());
+            for (CacheTrackerEvent event : events) {
+                processKey(event.getKey(), event.getType());
             }
         });
         QueryCursor<Cache.Entry<String, Heartbeat>> cursor = cacheInfo.get(ignite).query(query);
@@ -88,5 +98,38 @@ public class CacheLocalKeysTracker {
         ClusterNode localNode = ignite.cluster().localNode();
         int[] nodes = affinity.allPartitions(localNode);
         return Arrays.stream(nodes).boxed().collect(Collectors.toList());
+    }
+
+    private static class CacheTrackerEvent implements Serializable {
+        @NotNull
+        private final String key;
+        @NotNull
+        private final EventType type;
+
+        private CacheTrackerEvent(CacheEntryEvent<? extends String, ? extends Heartbeat> event) {
+            key = event.getKey();
+            type = event.getEventType();
+        }
+
+        public @NotNull String getKey() {
+            return key;
+        }
+
+        public @NotNull EventType getType() {
+            return type;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CacheTrackerEvent that = (CacheTrackerEvent) o;
+            return key.equals(that.key) && type == that.type;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, type);
+        }
     }
 }
